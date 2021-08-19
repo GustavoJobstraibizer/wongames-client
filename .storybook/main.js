83ccd4e2aef01ee62d6a2863b99ea869f0c6e04e
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2021-08-19 17:09:01 -0300

    feat: Alterando bundle webpack storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,5 +6,9 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-  ]
+  ],
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
+  }
 }
