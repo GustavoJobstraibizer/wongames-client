cf5331b3e5d802f71aa6386cabc16efb911e99c6
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2021-08-19 17:08:05 -0300

    feat: Incluindo ThemeProvider ao Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,12 @@
-import GlobalStyles from '../src/styles/global';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from 'styles/global';
+import theme from 'styles/theme';
 
 export const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Story />
-    </>
+    </ThemeProvider>
   ),
 ];
