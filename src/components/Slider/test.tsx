ccb5fd9306e09fb8ec0a7c376e68d94352f2422e
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2021-08-20 15:03:26 -0300

    feat: Component Slider / Storybook / tests

---
+++ src/components/Slider/test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react'
+import 'match-media-mock'
+import Slider from '.'
+
+describe('Slider', () => {
+  it('should render children as Slider item', () => {
+    const { container } = render(
+      <Slider settings={{ slidesToShow: 2, infinite: false }}>
+        <span>Item 1</span>
+        <span>Item 2</span>
+      </Slider>
+    )
+
+    const itemOne = screen.getByText(/item 1/i)
+    const itemTwo = screen.getByText(/item 2/i)
+
+    expect(itemOne).toBeInTheDocument()
+    expect(itemOne.parentElement?.parentElement).toHaveClass('slick-slide')
+
+    expect(itemTwo).toBeInTheDocument()
+    expect(itemTwo.parentElement?.parentElement).toHaveClass('slick-slide')
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
