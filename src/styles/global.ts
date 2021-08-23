7934589631addc5da77619fea91fe222b44f7b58
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2021-08-23 09:07:22 -0300

    feat: Definindo propriedade removeBg para GlobalStyles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,51 +1,72 @@
-import { createGlobalStyle, css } from 'styled-components'
+import {
+  createGlobalStyle,
+  css,
+  DefaultTheme,
+  GlobalStyleComponent
+} from 'styled-components'
 
-const GlobalStyles = createGlobalStyle`
-  @font-face {
-    font-family: 'Poppins';
-    font-style: normal;
-    font-weight: 300;
-    font-display: swap;
-    src: local(''),
-        url('/fonts/poppins-v15-latin-300.woff2') format('woff2'),
-  }
+type GlobalStyleProps = {
+  removeBg?: boolean
+}
 
-  @font-face {
-    font-family: 'Poppins';
-    font-style: normal;
-    font-weight: 400;
-    font-display: swap;
-    src: local(''),
-        url('/fonts/poppins-v15-latin-regular.woff2') format('woff2'),
-  }
+const GlobalStyles: GlobalStyleComponent<
+  GlobalStyleProps,
+  DefaultTheme
+> = createGlobalStyle`
 
-  @font-face {
-    font-family: 'Poppins';
-    font-style: normal;
-    font-weight: 600;
-    font-display: swap;
-    src: local(''),
-        url('/fonts/poppins-v15-latin-600.woff2') format('woff2'),
-  }
-
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-
-  ${({ theme }) => css`
-    html {
-      font-size: 62.5%;
+    @font-face {
+      font-family: 'Poppins';
+      font-style: normal;
+      font-weight: 300;
+      font-display: swap;
+      src: local(''), url('/fonts/poppins-v15-latin-300.woff2') format('woff2');
     }
 
-    body {
-      font-family: ${theme.font.family};
-      font-size: ${theme.font.sizes.medium};
+    @font-face {
+      font-family: 'Poppins';
+      font-style: normal;
+      font-weight: 400;
+      font-display: swap;
+      src: local(''),
+        url('/fonts/poppins-v15-latin-regular.woff2') format('woff2');
     }
-  `}
+
+    @font-face {
+      font-family: 'Poppins';
+      font-style: normal;
+      font-weight: 600;
+      font-display: swap;
+      src: local(''), url('/fonts/poppins-v15-latin-600.woff2') format('woff2');
+    }
+
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+
+      &:before,
+      &:after {
+        box-sizing: inherit;
+      }
+    }
+
+    ${({ theme, removeBg }) => css`
+      html {
+        font-size: 62.5%;
+      }
+
+      body {
+        font-family: ${theme.font.family};
+        font-size: ${theme.font.sizes.medium};
+
+        ${!removeBg &&
+        css`
+          background-color: ${theme.colors.mainBg};
+        `}
+      }
+    `}
 `
 
 export default GlobalStyles
