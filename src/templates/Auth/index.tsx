ade1a157b64307a2d5f9c0dcf382eae17c3c8a1e
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2021-08-25 11:42:17 -0300

    feat: Template Auth / tests

---
+++ src/templates/Auth/index.tsx
@@ -0,0 +1,45 @@
+import Heading from 'components/Heading'
+import Logo from 'components/Logo'
+import * as S from './styles'
+
+type AuthProps = {
+  title: string
+  children: React.ReactNode
+}
+
+const Auth = ({ title, children }: AuthProps) => {
+  return (
+    <S.Wrapper>
+      <S.BannerBlock>
+        <S.BannerContent>
+          <Logo id="banner" />
+
+          <div>
+            <Heading size="huge">All your favorite games in one place</Heading>
+            <S.Subtitle>
+              <strong>WON</strong> is the best and most complete gaming
+              platform.
+            </S.Subtitle>
+          </div>
+
+          <S.Footer>
+            Won Games {new Date().getFullYear()} Todos os Direitos Reservados
+          </S.Footer>
+        </S.BannerContent>
+      </S.BannerBlock>
+
+      <S.Content>
+        <S.ContentWrapper>
+          <Logo id="content-form" color="black" size="large" />
+          <Heading color="black" lineColor="secondary" lineLeft>
+            {title}
+          </Heading>
+
+          {children}
+        </S.ContentWrapper>
+      </S.Content>
+    </S.Wrapper>
+  )
+}
+
+export default Auth
---
+++ src/templates/Auth/test.tsx
@@ -0,0 +1,40 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/test-utils'
+import Auth from '.'
+
+describe('Auth', () => {
+  it('should render all components and children', () => {
+    renderWithTheme(
+      <Auth title="Auth">
+        <input type="text" />
+      </Auth>
+    )
+
+    expect(screen.getAllByLabelText(/won games/i)).toHaveLength(2)
+    expect(
+      screen.getByRole('heading', {
+        name: /All your favorite games in one place/i
+      })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', {
+        name: /WON is the best and most complete gaming platform/i
+      })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', {
+        name: /WON is the best and most complete gaming platform/i
+      })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', {
+        name: /auth/i
+      })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('textbox')).toBeInTheDocument()
+  })
+})
