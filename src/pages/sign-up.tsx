28c122e654cf0038bb3aee191f8deec9e77f4a59
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2021-08-26 08:48:06 -0300

    feat: Sign-in e Sign-up pages

---
+++ src/pages/sign-up.tsx
@@ -0,0 +1,10 @@
+import FormSignUp from 'components/FormSignUp'
+import Auth from 'templates/Auth'
+
+export default function SignUpPage() {
+  return (
+    <Auth title="Sign Up">
+      <FormSignUp />
+    </Auth>
+  )
+}
