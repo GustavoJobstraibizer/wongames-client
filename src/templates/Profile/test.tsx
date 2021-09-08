d98decdfdefdc2b23ee4bf0c2c513546592b29f3
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2021-09-08 15:57:12 -0300

    feat: Templates profile

---
+++ src/templates/Profile/test.tsx
@@ -0,0 +1,44 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/test-utils'
+import Profile from '.'
+
+jest.mock('next/router', () => ({
+  useRouter: jest.fn(() => ({ asPath: '/profile/me' }))
+}))
+
+jest.mock('templates/Base', () => {
+  return {
+    __esModule: true,
+    default: function Mock({ children }: { children: React.ReactNode }) {
+      return <div data-testid="mock base">{children}</div>
+    }
+  }
+})
+
+jest.mock('components/Heading', () => {
+  return {
+    __esModule: true,
+    default: function Mock({ children }: { children: React.ReactNode }) {
+      return <div data-testid="mock Heading">{children}</div>
+    }
+  }
+})
+
+jest.mock('components/ProfileMenu', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="mock ProfileMenu" />
+    }
+  }
+})
+
+describe('Profile', () => {
+  it('should render Profile sections', () => {
+    renderWithTheme(<Profile>Me</Profile>)
+
+    expect(screen.getByText('Me')).toBeInTheDocument()
+    expect(screen.getByText(/my profile/i)).toBeInTheDocument()
+    expect(screen.getByTestId('mock ProfileMenu')).toBeInTheDocument()
+  })
+})
