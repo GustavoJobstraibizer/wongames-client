7003cc1f2d7f880a2da8274c9cfe0b0016b3a4c8
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2021-09-13 16:37:49 -0300

    feat: Adicionando BannerFragment, GameFragment e HighlightFragment

---
+++ src/graphql/generated/QueryHome.ts
@@ -0,0 +1,62 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+import { ENUM_COMPONENTPAGERIBBON_COLOR, ENUM_COMPONENTPAGERIBBON_SIZE } from "./globalTypes";
+
+// ====================================================
+// GraphQL query operation: QueryHome
+// ====================================================
+
+export interface QueryHome_banners_image {
+  __typename: "UploadFile";
+  url: string;
+}
+
+export interface QueryHome_banners_button {
+  __typename: "ComponentPageButton";
+  label: string;
+  link: string;
+}
+
+export interface QueryHome_banners_ribbon {
+  __typename: "ComponentPageRibbon";
+  text: string | null;
+  color: ENUM_COMPONENTPAGERIBBON_COLOR | null;
+  size: ENUM_COMPONENTPAGERIBBON_SIZE | null;
+}
+
+export interface QueryHome_banners {
+  __typename: "Banner";
+  image: QueryHome_banners_image | null;
+  title: string;
+  subtitle: string;
+  button: QueryHome_banners_button | null;
+  ribbon: QueryHome_banners_ribbon | null;
+}
+
+export interface QueryHome_newGames_cover {
+  __typename: "UploadFile";
+  url: string;
+}
+
+export interface QueryHome_newGames_developers {
+  __typename: "Developer";
+  name: string;
+}
+
+export interface QueryHome_newGames {
+  __typename: "Game";
+  name: string;
+  release_date: any | null;
+  slug: string;
+  cover: QueryHome_newGames_cover | null;
+  developers: QueryHome_newGames_developers[];
+  price: number;
+}
+
+export interface QueryHome {
+  banners: QueryHome_banners[];
+  newGames: QueryHome_newGames[];
+}
