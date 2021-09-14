8b751d52499741500a730d7bdce765ff0d53352d
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2021-09-14 11:27:11 -0300

    feat: Recommended Games na pagina de cart

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,18 +1,29 @@
 import itemsMock from 'components/CartList/mock'
-import gamesMock from 'components/GameCardSlider/mock'
-import hightlightMock from 'components/Highlight/mock'
 import cardsMock from 'components/PaymentOptions/mock'
+import { QueryRecommended } from 'graphql/generated/QueryRecommended'
+import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
 import Cart, { CartProps } from 'templates/Cart'
+import { initializeApollo } from 'utils/apollo'
+import { gamesMapper, highlightMapper } from 'utils/mappers'
 
 export default function CartPage(props: CartProps) {
   return <Cart {...props} />
 }
 
 export async function getServerSideProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<QueryRecommended>({
+    query: QUERY_RECOMMENDED
+  })
+
   return {
     props: {
-      recommendedGames: gamesMock.slice(0, 5),
-      recommendedHighlight: hightlightMock,
+      recommendedTitle: data?.recommended?.section?.title,
+      recommendedGames: gamesMapper(data.recommended?.section?.games),
+      recommendedHighlight: highlightMapper(
+        data.recommended?.section?.highlight
+      ),
       items: itemsMock,
       total: 'R$ 400,00',
       cards: cardsMock
