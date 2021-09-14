a245e819f0bf03bf46e2ccf074d5953f442e9d80
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2021-09-14 10:33:10 -0300

    refactor: Criando mappers para os dados da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { QueryHome } from 'graphql/generated/QueryHome'
 import { QUERY_HOME } from 'graphql/queries/home'
 import Home, { HomeTemplateProps } from 'templates/Home'
 import { initializeApollo } from 'utils/apollo'
+import { bannerMapper, gamesMapper, highlightMapper } from 'utils/mappers'
 
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
@@ -17,77 +18,18 @@ export async function getStaticProps() {
   return {
     props: {
       revalidate: 60,
-      banners: banners.map((banner) => ({
-        img: `http://localhost:1337${banner.image?.url}`,
-        title: banner.title,
-        subtitle: banner.subtitle,
-        buttonLabel: banner.button?.label,
-        buttonLink: banner.button?.link,
-        ...(banner.ribbon && {
-          ribbon: banner.ribbon.text,
-          ribbonColor: banner.ribbon.color,
-          ribbonSize: banner.ribbon.size
-        })
-      })),
+      banners: bannerMapper(banners),
       newGamesTitle: sections?.newGames?.title,
-      newGames: newGames.map((game) => ({
-        title: game.name,
-        slug: game.slug,
-        developer: game.developers[0].name,
-        img: `http://localhost:1337${game.cover?.url}`,
-        price: game.price
-      })),
-      mostPopularHighlight: {
-        title: sections?.popularGames?.highlight?.title,
-        subtitle: sections?.popularGames?.highlight?.subtitle,
-        buttonLabel: sections?.popularGames?.highlight?.buttonLabel,
-        buttonLink: sections?.popularGames?.highlight?.buttonLink,
-        backgroundImage: `http://localhost:1337${sections?.popularGames?.highlight?.background?.url}`,
-        floatImage: `http://localhost:1337${sections?.popularGames?.highlight?.floatImage?.url}`,
-        alignment: sections?.popularGames?.highlight?.alignment
-      },
+      newGames: gamesMapper(newGames),
+      mostPopularHighlight: highlightMapper(sections?.popularGames?.highlight),
       mostPopularGamesTitle: sections?.popularGames?.title,
-      mostPopularGames: sections?.popularGames!.games.map((game) => ({
-        title: game.name,
-        slug: game.slug,
-        developer: game.developers[0].name,
-        img: `http://localhost:1337${game.cover?.url}`,
-        price: game.price
-      })),
+      mostPopularGames: gamesMapper(sections?.popularGames!.games),
       upcommingGamesTitle: sections?.upcommingGames?.title,
-      upcommingGames: upcommingGames.map((game) => ({
-        title: game.name,
-        slug: game.slug,
-        developer: game.developers[0].name,
-        img: `http://localhost:1337${game.cover?.url}`,
-        price: game.price
-      })),
-      upcommingHighlight: {
-        title: sections?.upcommingGames?.highlight?.title,
-        subtitle: sections?.upcommingGames?.highlight?.subtitle,
-        buttonLabel: sections?.upcommingGames?.highlight?.buttonLabel,
-        buttonLink: sections?.upcommingGames?.highlight?.buttonLink,
-        backgroundImage: `http://localhost:1337${sections?.upcommingGames?.highlight?.background?.url}`,
-        floatImage: `http://localhost:1337${sections?.upcommingGames?.highlight?.floatImage?.url}`,
-        alignment: sections?.upcommingGames?.highlight?.alignment
-      },
+      upcommingGames: gamesMapper(upcommingGames),
+      upcommingHighlight: highlightMapper(sections?.upcommingGames?.highlight),
       freeGamesTitle: sections?.freeGames?.title,
-      freeGames: freeGames.map((game) => ({
-        title: game.name,
-        slug: game.slug,
-        developer: game.developers[0].name,
-        img: `http://localhost:1337${game.cover?.url}`,
-        price: game.price
-      })),
-      freeHighlight: {
-        title: sections?.freeGames?.highlight?.title,
-        subtitle: sections?.freeGames?.highlight?.subtitle,
-        buttonLabel: sections?.freeGames?.highlight?.buttonLabel,
-        buttonLink: sections?.freeGames?.highlight?.buttonLink,
-        backgroundImage: `http://localhost:1337${sections?.freeGames?.highlight?.background?.url}`,
-        floatImage: `http://localhost:1337${sections?.freeGames?.highlight?.floatImage?.url}`,
-        alignment: sections?.freeGames?.highlight?.alignment
-      }
+      freeGames: gamesMapper(freeGames),
+      freeHighlight: highlightMapper(sections?.freeGames?.highlight)
     }
   }
 }
