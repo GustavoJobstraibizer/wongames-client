496d5701782f49373c0d0e74688235893e4a7c68
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2021-09-14 11:27:37 -0300

    feat: Recommended Games na pagina wishlist

--- src/pages/wishlist.tsx
+++ src/pages/wishlist.tsx
@@ -1,16 +1,28 @@
 import gamesMock from 'components/GameCardSlider/mock'
-import hightlightMock from 'components/Highlight/mock'
+import { QueryRecommended } from 'graphql/generated/QueryRecommended'
+import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
 import Wishlist, { WishlistTemplateProps } from 'templates/Wishlist'
+import { initializeApollo } from 'utils/apollo'
+import { gamesMapper, highlightMapper } from 'utils/mappers'
 
 export default function WishlistPage(props: WishlistTemplateProps) {
   return <Wishlist {...props} />
 }
 
 export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<QueryRecommended>({
+    query: QUERY_RECOMMENDED
+  })
+
   return {
     props: {
-      recommendedGames: gamesMock.slice(0, 5),
-      recommendedHighlight: hightlightMock,
+      recommendedTitle: data.recommended?.section?.title,
+      recommendedGames: gamesMapper(data.recommended?.section?.games),
+      recommendedHighlight: highlightMapper(
+        data.recommended?.section?.highlight
+      ),
       games: gamesMock
     }
   }
