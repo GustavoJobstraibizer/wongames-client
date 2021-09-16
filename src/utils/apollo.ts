2418e26831678f14a95ac45342bcec6b19ab4d4b
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2021-09-16 11:32:52 -0300

    refactor: Alterando type para receber valor null

--- src/utils/apollo.ts
+++ src/utils/apollo.ts
@@ -1,24 +1,20 @@
-import {
-  ApolloClient,
-  HttpLink,
-  InMemoryCache,
-  NormalizedCacheObject
-} from '@apollo/client'
+import { ApolloClient, HttpLink, NormalizedCacheObject } from '@apollo/client'
 import { useMemo } from 'react'
+import apolloCache from './apolloCache'
 
 const isSSR = typeof window === 'undefined'
 
-let apolloClient: ApolloClient<NormalizedCacheObject>
+let apolloClient: ApolloClient<NormalizedCacheObject | null>
 
 function createApolloClient() {
   return new ApolloClient({
     ssrMode: isSSR,
     link: new HttpLink({ uri: 'http://localhost:1337/graphql' }),
-    cache: new InMemoryCache()
+    cache: apolloCache
   })
 }
 
-export function initializeApollo(initialState = {}) {
+export function initializeApollo(initialState = null) {
   // Verifica se ja existe uma instancia, se nao existir
   // cria uma nova se existir, usa a instancia existente
   const apolloClientGlobal = apolloClient ?? createApolloClient()
@@ -36,7 +32,7 @@ export function initializeApollo(initialState = {}) {
   return apolloClient
 }
 
-export function useApollo(initialState = {}) {
+export function useApollo(initialState = null) {
   const store = useMemo(() => initializeApollo(initialState), [initialState])
   return store
 }
