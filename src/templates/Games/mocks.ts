f6b287dbf7c494ace38d7385ae89985bd23f0f3e
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2021-09-18 13:47:18 -0300

    feat: Adicionando mock para Games empty

--- src/templates/Games/mocks.ts
+++ src/templates/Games/mocks.ts
@@ -1,16 +1,34 @@
 import { QUERY_GAMES } from 'graphql/queries/games'
 
+export const noGamesMock = {
+  request: {
+    query: QUERY_GAMES,
+    variables: { limit: 10, where: {} }
+  },
+  result: {
+    data: {
+      games: [],
+      gamesConnection: {
+        values: [],
+        __typename: 'GameConnection'
+      }
+    }
+  }
+}
+
 export const gamesMock = {
   request: {
     query: QUERY_GAMES,
     variables: {
-      limit: 10
+      limit: 10,
+      where: {}
     }
   },
   result: {
     data: {
       games: [
         {
+          id: '1',
           name: 'First Game',
           slug: 'first-game',
           short_description: 'A short description',
@@ -21,7 +39,11 @@ export const gamesMock = {
           },
           __typename: 'Game'
         }
-      ]
+      ],
+      gamesConnection: {
+        values: [{ id: '1' }, { id: '2' }],
+        __typename: 'GameConnection'
+      }
     }
   }
 }
@@ -31,6 +53,7 @@ export const fetchMoreMock = {
     query: QUERY_GAMES,
     variables: {
       limit: 10,
+      where: {},
       start: 1
     }
   },
@@ -38,6 +61,7 @@ export const fetchMoreMock = {
     data: {
       games: [
         {
+          id: '2',
           name: 'fetch more Games',
           slug: 'fetch-more-games',
           short_description: 'A short description',
@@ -48,7 +72,11 @@ export const fetchMoreMock = {
           },
           __typename: 'Game'
         }
-      ]
+      ],
+      gamesConnection: {
+        values: [{ id: '1' }, { id: '2' }],
+        __typename: 'GameConnection'
+      }
     }
   }
 }
