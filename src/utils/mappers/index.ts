e567e8636fe30cf4378a6c258c804dd48f52738d
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2021-09-23 16:24:00 -0300

    feat: Adicionando mapper de cartMapper

--- src/utils/mappers/index.ts
+++ src/utils/mappers/index.ts
@@ -3,6 +3,7 @@ import {
   QueryHome_banners,
   QueryHome_sections_freeGames_highlight
 } from 'graphql/generated/QueryHome'
+import { formatPrice } from 'utils/formatPrice'
 
 export const bannerMapper = (banners: QueryHome_banners[]) => {
   return banners.map((banner) => ({
@@ -46,4 +47,15 @@ export const highlightMapper = (
       alignment: highlight?.alignment
     }
   )
+}
+
+export const cartMapper = (games: QueryGames_games[] | null | undefined) => {
+  return games
+    ? games.map((game) => ({
+        id: game.id,
+        title: game.name,
+        img: `http://localhost:1337${game.cover?.url}`,
+        price: formatPrice(game.price)
+      }))
+    : []
 }
--- src/utils/mappers/mappers.test.ts
+++ src/utils/mappers/mappers.test.ts
@@ -1,6 +1,7 @@
 import { bannerMapper, gamesMapper, highlightMapper } from '.'
 import { QueryGames_games } from './../../graphql/generated/QueryGames'
 import { QueryHome_banners } from './../../graphql/generated/QueryHome'
+import { cartMapper } from './index'
 
 describe('bannerMapper', () => {
   it('should return the formatted value correctly', () => {
@@ -82,5 +83,31 @@ describe('highlightMapper', () => {
       floatImage: 'http://localhost:1337/image/float-image.png',
       alignment: 'right'
     })
+  })
+})
+
+describe('cartMapper', () => {
+  it('should return an empty array if there are no games', () => {
+    expect(cartMapper(null)).toStrictEqual([])
+  })
+
+  it('should return the formatted value correctly', () => {
+    const game = {
+      id: '1',
+      name: 'game',
+      cover: {
+        url: '/image.png'
+      },
+      price: 100
+    } as QueryGames_games
+
+    expect(cartMapper([game])).toStrictEqual([
+      {
+        id: '1',
+        title: 'game',
+        price: '$100.00',
+        img: 'http://localhost:1337/image.png'
+      }
+    ])
   })
 })
