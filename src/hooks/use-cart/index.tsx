9d17621dccc7dd38a4411396f770051dc2835ed3
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2021-09-23 16:24:41 -0300

    feat: useCart Context / mock / tests

---
+++ src/hooks/use-cart/index.tsx
@@ -0,0 +1,112 @@
+import { useQueryGames } from 'graphql/queries/games'
+import { createContext, useContext, useEffect, useState } from 'react'
+import { formatPrice } from 'utils/formatPrice'
+import { getStorageItem, setStorageItem } from 'utils/localStorage'
+import { cartMapper } from 'utils/mappers'
+
+const CART_KEY = 'cartItems'
+
+type CartItem = {
+  id: string
+  img: string
+  title: string
+  price: string
+}
+
+export type CartContextData = {
+  items: CartItem[]
+  quantity: number
+  total: string
+  isInCart: (id: string) => boolean
+  addToCart: (id: string) => void
+  removeFromCart: (id: string) => void
+  clearCart: () => void
+  loading: boolean
+}
+
+export const CartContextDefaultValues = {
+  items: [],
+  quantity: 0,
+  total: '$0.00',
+  isInCart: () => false,
+  addToCart: () => null,
+  removeFromCart: () => null,
+  clearCart: () => null,
+  loading: false
+}
+
+export const CartContext = createContext<CartContextData>(
+  CartContextDefaultValues
+)
+
+export type CartProviderProps = {
+  children: React.ReactNode
+}
+
+const CartProvider = ({ children }: CartProviderProps) => {
+  const [cartItems, setCartItems] = useState<string[]>([])
+
+  useEffect(() => {
+    const data = getStorageItem(CART_KEY)
+
+    if (data) {
+      setCartItems(data)
+    }
+  }, [])
+
+  const { data, loading } = useQueryGames({
+    skip: !cartItems.length,
+    variables: {
+      where: { id: cartItems }
+    }
+  })
+
+  const total = data?.games.reduce((acc, game) => (acc += game.price), 0) || 0
+
+  const isInCart = (id: string) => cartItems.includes(id)
+
+  const saveCart = (cartItems: string[]) => {
+    setCartItems(cartItems)
+    setStorageItem(CART_KEY, cartItems)
+  }
+
+  const addToCart = (id: string) => {
+    if (isInCart(id)) {
+      removeFromCart(id)
+      return
+    }
+
+    saveCart([...cartItems, id])
+  }
+
+  const removeFromCart = (id: string) => {
+    const newCartItems = cartItems.filter((item) => item !== id)
+
+    saveCart(newCartItems)
+  }
+
+  const clearCart = () => {
+    saveCart([])
+  }
+
+  return (
+    <CartContext.Provider
+      value={{
+        items: cartMapper(data?.games),
+        quantity: cartItems.length,
+        total: formatPrice(total),
+        isInCart,
+        addToCart,
+        removeFromCart,
+        clearCart,
+        loading
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  )
+}
+
+const useCart = () => useContext(CartContext)
+
+export { CartProvider, useCart }
