9f5651e20a3c6d81b3f0a457f2cf5c6dbd373f62
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2021-09-27 10:58:01 -0300

    feat: Utilizando o componente de Loader

--- src/components/CartList/index.tsx
+++ src/components/CartList/index.tsx
@@ -1,6 +1,7 @@
 import Button from 'components/Button'
 import Empty from 'components/Empty'
 import GameItem from 'components/GameItem'
+import Loader from 'components/Loader'
 import { useCart } from 'hooks/use-cart'
 import Link from 'next/link'
 import * as S from './styles'
@@ -10,15 +11,25 @@ export type CartListProps = {
 }
 
 const CartList = ({ hasButton = false }: CartListProps) => {
-  const { items, total } = useCart()
+  const { items, total, loading } = useCart()
+
+  if (loading) {
+    return (
+      <S.Loading>
+        <Loader />
+      </S.Loading>
+    )
+  }
 
   return (
     <S.Wrapper isEmpty={!items.length}>
       {items.length ? (
         <>
-          {items.map((item) => (
-            <GameItem key={item.title} {...item} />
-          ))}
+          <S.GamesList>
+            {items.map((item) => (
+              <GameItem key={item.title} {...item} />
+            ))}
+          </S.GamesList>
 
           <S.Footer>
             {!hasButton && <span>Total:</span>}
