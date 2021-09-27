06a1246940bcb55e361cbc28e877b398cb9b953d
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2021-09-27 13:50:45 -0300

    feat: Utilizando register mutation

--- src/components/FormSignUp/index.tsx
+++ src/components/FormSignUp/index.tsx
@@ -1,39 +1,63 @@
+import { useMutation } from '@apollo/client'
 import { AccountCircle, Email, Lock } from '@styled-icons/material-outlined'
 import Button from 'components/Button'
 import { FormLink, FormWrapper } from 'components/Form'
 import TextField from 'components/TextField'
+import { UsersPermissionsRegisterInput } from 'graphql/generated/globalTypes'
+import { MUTATION_REGISTER } from 'graphql/mutations/register'
 import Link from 'next/link'
+import React, { useState } from 'react'
 
 const FormSignUp = () => {
+  const [values, setValues] = useState<UsersPermissionsRegisterInput>({
+    email: '',
+    password: '',
+    username: ''
+  })
+
+  const [createUser] = useMutation(MUTATION_REGISTER)
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    await createUser({ variables: { input: values } })
+  }
+
+  const handleInput = (field: string, value: string | undefined) => {
+    setValues((previousValue) => ({ ...previousValue, [field]: value }))
+  }
+
   return (
     <FormWrapper>
-      <form>
+      <form onSubmit={handleSubmit}>
         <TextField
-          name="name"
-          placeholder="Name"
+          name="username"
+          placeholder="Username"
           type="text"
           icon={<AccountCircle />}
+          onInputChange={(v) => handleInput('username', v)}
         />
         <TextField
           name="email"
           placeholder="Email"
           type="email"
           icon={<Email />}
+          onInputChange={(v) => handleInput('email', v)}
         />
         <TextField
           name="password"
           placeholder="Password"
           type="password"
           icon={<Lock />}
+          onInputChange={(v) => handleInput('password', v)}
         />
         <TextField
           name="confirm-password"
           placeholder="Confirm Password"
-          type="confirm-password"
+          type="password"
           icon={<Lock />}
         />
 
-        <Button size="large" fullWidth>
+        <Button type="submit" size="large" fullWidth>
           Sign up now
         </Button>
 
--- src/components/FormSignUp/test.tsx
+++ src/components/FormSignUp/test.tsx
@@ -1,9 +1,14 @@
+import { MockedProvider } from '@apollo/client/testing'
 import { render, screen } from 'utils/test-utils'
 import FormSignUp from '.'
 
 describe('FormSignUp', () => {
   it('should render the form', () => {
-    const { container } = render(<FormSignUp />)
+    const { container } = render(
+      <MockedProvider>
+        <FormSignUp />
+      </MockedProvider>
+    )
 
     expect(screen.getByPlaceholderText(/name/i)).toBeInTheDocument()
     expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
@@ -17,7 +22,11 @@ describe('FormSignUp', () => {
   })
 
   it('should render the text and link to sign in', () => {
-    render(<FormSignUp />)
+    render(
+      <MockedProvider>
+        <FormSignUp />
+      </MockedProvider>
+    )
 
     expect(screen.getByRole('link', { name: /sign in/i })).toBeInTheDocument()
     expect(screen.getByText(/already have an account\?/i)).toBeInTheDocument()
