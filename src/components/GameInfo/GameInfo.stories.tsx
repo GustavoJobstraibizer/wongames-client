2c971d062c871cf6abf88ef746f1e1bbc17d0fd8
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2021-09-27 10:42:51 -0300

    feat: Adicionando story para produtos no carrinho

--- src/components/GameInfo/GameInfo.stories.tsx
+++ src/components/GameInfo/GameInfo.stories.tsx
@@ -1,4 +1,5 @@
 import { Meta, Story } from '@storybook/react'
+import { CartContextData } from 'hooks/use-cart'
 import GameInfo, { GameInfoProps } from '.'
 import mockInfoGame from './mock'
 
@@ -18,3 +19,13 @@ export const Basic: Story<GameInfoProps> = (args) => (
     <GameInfo {...args} />
   </div>
 )
+
+export const IsInCart: Story<GameInfoProps & CartContextData> = (args) => (
+  <div style={{ maxWidth: '144rem', padding: '1.5rem', margin: '0 auto' }}>
+    <GameInfo {...args} />
+  </div>
+)
+
+IsInCart.args = {
+  isInCart: () => true
+}
