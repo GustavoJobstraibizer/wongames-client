ceb15906008349e6805d5d72b3fa63312bd42c6c
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2021-09-27 17:09:35 -0300

    test: Mock next/router

--- src/components/FormSignUp/test.tsx
+++ src/components/FormSignUp/test.tsx
@@ -2,6 +2,17 @@ import { MockedProvider } from '@apollo/client/testing'
 import { render, screen } from 'utils/test-utils'
 import FormSignUp from '.'
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const useRouter = jest.spyOn(require('next/router'), 'useRouter')
+const push = jest.fn()
+
+useRouter.mockImplementation(() => ({
+  push,
+  query: '',
+  asPath: '',
+  route: ''
+}))
+
 describe('FormSignUp', () => {
   it('should render the form', () => {
     const { container } = render(
