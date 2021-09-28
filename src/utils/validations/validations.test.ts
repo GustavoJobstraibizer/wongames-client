3d815ad7b28a22e2b339dabd6c041e22d4b7830b
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2021-09-28 16:28:55 -0300

    feat: Validations para forgotPassword e resetPassword

--- src/utils/validations/validations.test.ts
+++ src/utils/validations/validations.test.ts
@@ -1,4 +1,9 @@
-import { signInValidate, signUpValidate } from '.'
+import {
+  forgotValidate,
+  resetValidate,
+  signInValidate,
+  signUpValidate
+} from '.'
 
 describe('validations', () => {
   describe('SignInValidate()', () => {
@@ -22,18 +27,27 @@ describe('validations', () => {
 
   describe('SignUpValidate()', () => {
     it('should validate empty fields', () => {
-      const values = { username: '', email: '', password: '' }
+      const values = {
+        username: '',
+        email: '',
+        password: '',
+        confirm_password: ''
+      }
 
       expect(signUpValidate(values)).toMatchObject({
         username: expect.any(String),
         email: expect.any(String),
-        password: expect.any(String),
-        confirm_password: expect.any(String)
+        password: expect.any(String)
       })
     })
 
     it('should return short username error', () => {
-      const values = { username: 'a', email: '', password: '' }
+      const values = {
+        username: 'a',
+        email: '',
+        password: '',
+        confirm_password: ''
+      }
 
       expect(signUpValidate(values).username).toMatchInlineSnapshot(
         `"\\"username\\" length must be at least 5 characters long"`
@@ -41,7 +55,12 @@ describe('validations', () => {
     })
 
     it('should return invalid email error', () => {
-      const values = { username: '', email: 'invalid', password: '' }
+      const values = {
+        username: '',
+        email: 'invalid',
+        password: '',
+        confirm_password: ''
+      }
 
       expect(signUpValidate(values).email).toMatchInlineSnapshot(
         `"\\"email\\" must be a valid email"`
@@ -57,6 +76,59 @@ describe('validations', () => {
       }
 
       expect(signUpValidate(values).confirm_password).toMatchInlineSnapshot(
+        `"confirm password does not match with password"`
+      )
+    })
+  })
+
+  describe('forgotValidate', () => {
+    it('should validate empty fields', () => {
+      const values = { email: '' }
+
+      expect(forgotValidate(values)).toMatchObject({
+        email: '"email" is not allowed to be empty'
+      })
+    })
+
+    it('should return invalid email error', () => {
+      const values = { email: 'invalid' }
+
+      expect(forgotValidate(values).email).toMatchInlineSnapshot(
+        `"\\"email\\" must be a valid email"`
+      )
+    })
+  })
+
+  describe('resetValidate', () => {
+    it('should validate empty fields', () => {
+      const values = {
+        password: '',
+        confirm_password: ''
+      }
+
+      expect(resetValidate(values)).toMatchObject({
+        password: expect.any(String)
+      })
+    })
+
+    it('should validate confirm_password', () => {
+      const values = {
+        password: '123',
+        confirm_password: ''
+      }
+
+      expect(resetValidate(values).confirm_password).toMatchInlineSnapshot(
+        `"\\"confirm_password\\" is not allowed to be empty"`
+      )
+    })
+
+    it('should validate confirm_password when different', () => {
+      const values = {
+        password: '123',
+        confirm_password: '321'
+      }
+
+      expect(resetValidate(values).confirm_password).toMatchInlineSnapshot(
         `"confirm password does not match with password"`
       )
     })
