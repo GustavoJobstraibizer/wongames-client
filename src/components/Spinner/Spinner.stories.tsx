6ec52a394ed45abb8ae8ad4c9ab0eab0797bede2
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2021-10-20 21:53:34 -0300

    chore: Spinner component

---
+++ src/components/Spinner/Spinner.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, Story } from '@storybook/react/types-6-0'
+import Spinner, { SpinnerProps } from '.'
+
+export default {
+  title: 'Spinner',
+  component: Spinner
+} as Meta
+
+export const Default: Story<SpinnerProps> = (args) => <Spinner {...args} />
