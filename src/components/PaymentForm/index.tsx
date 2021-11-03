5231c092919a7d49b8c1f3ceb7d60fff2a6d1e2b
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2021-11-03 14:37:42 -0300

    chore: Metodos para jogos pagos e free

--- src/components/PaymentForm/index.tsx
+++ src/components/PaymentForm/index.tsx
@@ -1,14 +1,95 @@
-import { CardElement } from '@stripe/react-stripe-js'
-import { StripeCardElementChangeEvent } from '@stripe/stripe-js'
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js'
+import { PaymentIntent, StripeCardElementChangeEvent } from '@stripe/stripe-js'
 import { ErrorOutline, ShoppingCart } from '@styled-icons/material-outlined'
 import Button from 'components/Button'
+import { FormLoading } from 'components/Form'
 import Heading from 'components/Heading'
-import { useState } from 'react'
+import { useCart } from 'hooks/use-cart'
+import { Session } from 'next-auth'
+import { useRouter } from 'next/router'
+import { FormEvent, useEffect, useState } from 'react'
+import { createPayment, createPaymentIntent } from 'utils/stripe/methods'
 import * as S from './styles'
 
-const PaymentForm = () => {
+type PaymentFormProps = {
+  session: Session
+}
+
+const PaymentForm = ({ session }: PaymentFormProps) => {
+  const { items } = useCart()
+  const { push } = useRouter()
+  const stripe = useStripe()
+  const elements = useElements()
+
   const [error, setError] = useState<string | null>(null)
+  const [loading, setLoading] = useState<boolean>(false)
   const [disabled, setDisabled] = useState(true)
+  const [clientSecret, setClientSecret] = useState('')
+  const [freeGames, setFreeGames] = useState(false)
+
+  const saveOrder = async (paymentIntent?: PaymentIntent) => {
+    const data = await createPayment({
+      items,
+      paymentIntent,
+      token: session.jwt
+    })
+
+    return data
+  }
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault()
+
+    setLoading(true)
+
+    if (freeGames) {
+      saveOrder()
+      push('/success')
+      return
+    }
+
+    const payload = await stripe?.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements!.getElement(CardElement)!
+      }
+    })
+
+    if (payload?.error) {
+      setError(`Payment failed ${payload?.error?.message}`)
+      setLoading(false)
+      return
+    }
+
+    setError(null)
+    setLoading(false)
+
+    saveOrder(payload?.paymentIntent)
+    console.log('Compra efetuada com sucesso')
+    push('/success')
+  }
+
+  useEffect(() => {
+    async function setPaymentMode() {
+      if (items.length) {
+        const data = await createPaymentIntent({ token: session.jwt, items })
+
+        if (data.freeGames) {
+          setFreeGames(true)
+          return
+        }
+
+        if (data.error) {
+          setError(data.error)
+          return
+        }
+
+        setFreeGames(false)
+        setClientSecret(data.client_secret)
+      }
+    }
+
+    setPaymentMode()
+  }, [items, session])
 
   const handleChange = async (event: StripeCardElementChangeEvent) => {
     setDisabled(event.empty)
@@ -17,40 +98,47 @@ const PaymentForm = () => {
 
   return (
     <S.Wrapper>
-      <S.Body>
-        <Heading color="black" size="small" lineBottom>
-          Payment
-        </Heading>
+      <form onSubmit={handleSubmit}>
+        <S.Body>
+          <Heading color="black" size="small" lineBottom>
+            Payment
+          </Heading>
 
-        <CardElement
-          onChange={handleChange}
-          options={{
-            hidePostalCode: true,
-            style: { base: { fontSize: '16px' } }
-          }}
-        />
+          {freeGames ? (
+            <S.FreeGames>Only free games, click buy and enjoy!</S.FreeGames>
+          ) : (
+            <CardElement
+              onChange={handleChange}
+              options={{
+                hidePostalCode: true,
+                style: { base: { fontSize: '16px' } }
+              }}
+            />
+          )}
 
-        {error && (
-          <S.Error>
-            <ErrorOutline size={20} />
-            {error}
-          </S.Error>
-        )}
-      </S.Body>
+          {error && (
+            <S.Error>
+              <ErrorOutline size={20} />
+              {error}
+            </S.Error>
+          )}
+        </S.Body>
 
-      <S.Footer>
-        <Button as="a" fullWidth minimal>
-          Continue shopping
-        </Button>
+        <S.Footer>
+          <Button as="a" fullWidth minimal>
+            Continue shopping
+          </Button>
 
-        <Button
-          fullWidth
-          icon={<ShoppingCart />}
-          disabled={disabled || !!error}
-        >
-          Buy now
-        </Button>
-      </S.Footer>
+          <Button
+            fullWidth
+            type="submit"
+            icon={loading ? <FormLoading /> : <ShoppingCart />}
+            disabled={!freeGames && (disabled || !!error)}
+          >
+            {!loading && <span>Buy now</span>}
+          </Button>
+        </S.Footer>
+      </form>
     </S.Wrapper>
   )
 }
