e1b7677b8a4ab8d68e5869093a0d40b37c0d177d
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2021-11-03 15:43:26 -0300

    chore: Orders Mapper

--- src/utils/mappers/index.ts
+++ src/utils/mappers/index.ts
@@ -4,6 +4,7 @@ import {
   QueryHome_sections_freeGames_highlight
 } from 'graphql/generated/QueryHome'
 import { formatPrice } from 'utils/formatPrice'
+import { queryOrders_orders } from './../../graphql/generated/queryOrders'
 import { QueryWishlist_wishlists_games } from './../../graphql/generated/QueryWishlist'
 
 export const bannerMapper = (banners: QueryHome_banners[]) => {
@@ -60,5 +61,37 @@ export const cartMapper = (games: QueryGames_games[] | null | undefined) => {
         img: `http://localhost:1337${game.cover?.url}`,
         price: formatPrice(game.price)
       }))
+    : []
+}
+
+export const ordersMapper = (orders: queryOrders_orders[]) => {
+  return orders
+    ? orders.map((order) => {
+        return {
+          id: order.id,
+          paymentInfo: {
+            flag: order.card_brand,
+            img: order.card_brand ? `/img/cards/${order.card_brand}.png` : null,
+            number: order.card_last4
+              ? `**** **** **** ${order.card_last4}`
+              : 'Free Game',
+            purchaseDate: `Purchase made on ${new Intl.DateTimeFormat('en-US', {
+              day: 'numeric',
+              month: 'short',
+              year: 'numeric'
+            }).format(new Date(order.created_at))}`
+          },
+          games: order.games.map((game) => {
+            return {
+              id: game.id,
+              title: game.name,
+              downloadLink:
+                'https://wongames.com/game/download/yuYT56Tgh431LkjhNBgdf',
+              img: `http://localhost:1337${game.cover?.url}`,
+              price: formatPrice(game.price)
+            }
+          })
+        }
+      })
     : []
 }
