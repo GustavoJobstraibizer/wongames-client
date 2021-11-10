04edac75deecca884f79b73be5136247f24d76ac
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2021-11-10 11:23:53 -0300

    chore(cypress): utilizando cy.wait

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -7,6 +7,12 @@ describe('Home', () => {
     cy.get('.slick-slider').within(() => {
       cy.findByRole('heading', { name: /trine ultimate collection/i })
       cy.findByRole('link', { name: /buy now/i })
+
+      cy.get('.slick-dots > :nth-child(2) > button').click()
+      cy.wait(500)
+
+      cy.findByRole('heading', { name: /fallout/i })
+      cy.findByRole('link', { name: /buy now/i })
     })
   })
 })
