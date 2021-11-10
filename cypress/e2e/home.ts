765182367fb613bf8f17c1a220dad9816bfcce95
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2021-11-10 11:08:32 -0300

    chore(cypress): Utilizando findByRole e within

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -3,5 +3,10 @@
 describe('Home', () => {
   it('should render Home sections', () => {
     cy.visit('/');
+
+    cy.get('.slick-slider').within(() => {
+      cy.findByRole('heading', { name: /trine ultimate collection/i })
+      cy.findByRole('link', { name: /buy now/i })
+    })
   })
 })
