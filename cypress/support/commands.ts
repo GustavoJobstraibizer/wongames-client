5128593e73f8d41e0faf11fa27669e5a067f3010
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2021-11-10 09:56:04 -0300

    chore(Cypress): configurando testing-library

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -23,6 +23,10 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+// Add Testing Library commands
+import '@testing-library/cypress/add-commands';
+
 Cypress.Commands.add('google', () => {
   cy.visit('https://google.com')
 })
