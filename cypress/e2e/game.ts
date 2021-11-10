c29c3688e806a089a7c1ed08f0c688f2c4c58ba9
Gustavo h. Jobstraibizer <gustavo.henrique.ext@infracommerce.com.br>
2021-11-10 15:11:01 -0300

    chore(cypress): Tests GameDetails e Showcase

--- cypress/e2e/game.ts
+++ cypress/e2e/game.ts
@@ -21,5 +21,27 @@ describe('Game', () => {
     })
 
     cy.getByDataCy('text-content').children().should('have.length.at.least', 2)
+
+    // game details
+    cy.getByDataCy('game-details').within(() => {
+      cy.findByRole('heading', { name: /game details/i }).should('exist')
+
+      cy.findByRole('heading', { name: /developer/i }).should('exist')
+      cy.findByRole('heading', { name: /release date/i }).should('exist')
+      cy.findByRole('heading', { name: /platforms/i }).should('exist')
+      cy.findByRole('heading', { name: /publishers/i }).should('exist')
+      cy.findByRole('heading', { name: /ratings/i }).should('exist')
+      cy.findByRole('heading', { name: /genres/i }).should('exist')
+
+
+      cy.findByText('Interplay').should('exist')
+      cy.findByText(/Sep 28, 1997/i).should('exist')
+      cy.findByRole('img', { name: /windows/i }).should('exist')
+      cy.findByText(/free/i).should('exist')
+      cy.findByText('Role-playing / Sci-fi / Turn-based').should('exist')
+    })
+
+    cy.shouldRenderShowcase({ name: 'Upcomming Games', highlight: true })
+    cy.shouldRenderShowcase({ name: 'You may like these Games!!!', highlight: false })
   })
 })
